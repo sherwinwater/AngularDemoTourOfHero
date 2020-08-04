3f16e6005e1c7204fc2c9abb5fca2d45fdd81429
sherwin <sherwin.water@gmail.com>
2020-08-03 23:25:19 -0400

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,50 @@
+import { Hero } from '../hero';
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService} from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  selectedHero: Hero;
+  heroes: Hero[];
+  
+  constructor(private heroService: HeroService,
+    private messageService:MessageService) {}
+
+    ngOnInit() {
+      this.getHeroes();
+    }
+
+  onSelect(hero:Hero): void{
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+  
+  // heroes = HEROES;
+  // constructor() { }
+
+  // ngOnInit(): void {
+  // }
+
+
+  // This asynchronous approach 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  // synchronous
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+}
+
+
+
